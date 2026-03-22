803b379b7ea75f24b053b763cc84d930663902e7
vishal-vr <vvr66486@gmail.com>
2026-03-22 08:52:39 +0530

    Delete combinational/adders directory

--- combinational/adders/half_adder.v
+++
@@ -1,4 +0,0 @@
-module half_adder_df(input a,b, output sum,carry);
-assign sum = a ^ b;
-assign carry = a & b;
-endmodule
--- combinational/adders/half_adder_b.v
+++
@@ -1,6 +0,0 @@
-module half_adder_beh(input a,b, output reg sum,carry);
-always @(*) begin
-    sum = a ^ b;
-    carry = a & b;
-end
-endmodule
